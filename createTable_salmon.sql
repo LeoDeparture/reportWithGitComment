d25daf21545c1321a084469a8d8900f904c8493f
LeoDeparture <53247108+LeoDeparture@users.noreply.github.com>
2020-08-18 15:12:58 +0800

    Update createTable_salmon.sql

--- createTable_salmon.sql
+++ createTable_salmon.sql
@@ -1,44 +1,14 @@
-# 以下面的建表语句为标准，创建29张表，对应serviceDict.json中的各项服务
+# 以下面的建表语句为标准，创建若干张表，对应serviceDict.json中的各项服务
 # salmon
-CREATE TABLE IF NOT EXISTS `mis-website-service-dev-fargate`(
+CREATE TABLE IF NOT EXISTS `service-name`(
 `buildNumber` INT UNSIGNED,
-`serviceName` VARCHAR(100) DEFAULT 'mis-website-service-dev-fargate',
-`taskFamily` VARCHAR(100) DEFAULT 'mis-website-task-dev-fargate',
-`clusterName` VARCHAR(100) DEFAULT 'zhiwen-cluster-dev',
+`serviceName` VARCHAR(100) DEFAULT 'service-name',
+`taskFamily` VARCHAR(100) DEFAULT 'task-name',
+`clusterName` VARCHAR(100) DEFAULT 'cluster-name',
 `imageTag` VARCHAR(100) DEFAULT 'DEV',
 `branchName` VARCHAR(100) DEFAULT 'develop',
-`ecRegistry` VARCHAR(100) DEFAULT 'https://412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn',
-`ecrRepo` VARCHAR(100) DEFAULT '412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn/salmon',
-`taskRevision` INT UNSIGNED,
-`gitHash` VARCHAR(100),
-`gitComment` VARCHAR(1000),
-PRIMARY KEY ( `buildNumber` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `mis-website-service-staging-fargate`(
-`buildNumber` INT UNSIGNED,
-`serviceName` VARCHAR(100) DEFAULT 'mis-website-service-staging-fargate',
-`taskFamily` VARCHAR(100) DEFAULT 'mis-website-task-staging-fargate',
-`clusterName` VARCHAR(100) DEFAULT 'zhiwen-cluster-dev',
-`imageTag` VARCHAR(100) DEFAULT 'STAGING',
-`branchName` VARCHAR(100) DEFAULT 'release',
-`ecRegistry` VARCHAR(100) DEFAULT 'https://412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn',
-`ecrRepo` VARCHAR(100) DEFAULT '412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn/salmon',
-`taskRevision` INT UNSIGNED,
-`gitHash` VARCHAR(100),
-`gitComment` VARCHAR(1000),
-PRIMARY KEY ( `buildNumber` )
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-CREATE TABLE IF NOT EXISTS `mis-website-service-fargate`(
-`buildNumber` INT UNSIGNED,
-`serviceName` VARCHAR(100) DEFAULT 'mis-website-service-fargate',
-`taskFamily` VARCHAR(100) DEFAULT 'mis-website-task-fargate',
-`clusterName` VARCHAR(100) DEFAULT 'zhiwen-cluster',
-`imageTag` VARCHAR(100) DEFAULT 'PROD',
-`branchName` VARCHAR(100) DEFAULT 'master',
-`ecRegistry` VARCHAR(100) DEFAULT 'https://412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn',
-`ecrRepo` VARCHAR(100) DEFAULT '412934042350.dkr.ecr.cn-northwest-1.amazonaws.com.cn/salmon',
+`ecRegistry` VARCHAR(100) DEFAULT 'https://***.dkr.ecr.cn-northwest-1.amazonaws.com.cn',
+`ecrRepo` VARCHAR(100) DEFAULT '***.dkr.ecr.cn-northwest-1.amazonaws.com.cn/salmon',
 `taskRevision` INT UNSIGNED,
 `gitHash` VARCHAR(100),
 `gitComment` VARCHAR(1000),
